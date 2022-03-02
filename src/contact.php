b903f757c1c72abbd2665c87a543534df24fde82
LaurenKScott <lkscottcom@gmail.com>
2022-03-02 16:36:12 -0500

    update contact.php

--- src/contact.php
+++ src/contact.php
@@ -11,7 +11,7 @@
         education on the South Shore since 1989">
         <meta name="robots" content="index, follow">
         <title>Contact Us - Busy Bee Child Care & Preschool</title>
-        <link rel="icon" href="/assets/beeicontransparent.png">
+        <link rel="icon" href="/assets/bee-icon.jpg">
         <!-- BOOTSTRAP STYLESHEET AND SCRIPT LINKS (CDN)-->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
@@ -19,27 +19,26 @@
     </head>
 
     <!-- BODY: THE START OF THE PAGE CONTENT -->
-    <body>
+    <body class="container-fluid" style="">
 
         <!-- PHP INFORMATION AND CONTENT -->
         <?php include("formhandler.php");?>
-        <div class="container-fluid">
         <a class="sr-only sr-only-focusable visually-hidden" aria-label="skip navigation" href="#main">
             Skip to main content</a>
          <!-- HEADER: LOGO, NAV -->
-        <header class="container-fluid position-sticky">
+        <header class="container-fluid position-sticky" style="">
             <!-- NAVIGATION BAR WITH STICKY POSITION-->
-            <nav role="navigation" class="navbar navbar-expand-sm navbar-light bg-light">
+            <nav role="navigation" class="navbar navbar-expand-sm navbar-light">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="index.html">
-                        <img src="assets/busybeelogo.png" alt="Busy Bee Logo" 
+                        <img src="/assets/busybeelogo.png" alt="Busy Bee Logo" 
                         style="max-width:100%; height:auto;">
                     </a>
                     <button class="navbar-toggler mx-auto" type="button" data-bs-toggle="collapse" 
                     data-bs-target="#collapsible-navbar">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse justify-content-end" 
+                    <div class="collapse navbar-collapse justify-content-end align-bottom"  
                     id="collapsible-navbar">
                         <ul class="navbar-nav" role="navigation">
                             <li class="nav-item px-1">
@@ -52,7 +51,7 @@
                                 <a class="nav-link" href="gallery.html">Gallery</a>
                             </li>
                             <li class="nav-item px-1">
-                                <a class="nav-link" href="parents.html">For Parents</a>
+                                <a class="nav-link" href="parents.html">Parents</a>
                             </li>
                             <li class="nav-item px-1">
                                 <a class="nav-link" href="faq.html">FAQ</a>
@@ -71,10 +70,10 @@
         <main>
             <div class="text-center"><h1>Don't Bee a Stranger!</h1></div>
             <form title="Contact Form" class="p-5 mb-3" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" 
-            method="POST" autocomplete="off">
-                <div><h2>Contact Us</h2></div>
+            method="POST" autocomplete="off" style="background-color:#FFCC33; color:">
+                <div class="text-center m-0 p-0"><h2 style="font-size:2em;">Contact Us</h2></div>
                 <div class="form-floating" id="form-info">
-                    <p><em>* All fields are required</em></p>
+                    <p class="text-center"><em>* All fields are required</em></p>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="name" name="name"
@@ -107,27 +106,26 @@
 
             </div>
         </main>
-        <footer>
-        <div class="btn-group mx-auto" role="group" aria-label="social media links">
-            <a class="btn" role="button"
-            href="https://www.facebook.com/BusyBeeChildCareMA" aria-label="facebook">
-                <i class="bi-facebook" style="font-size:36px; color:black;"></i>
-            </a>
-            <a class="btn" role="button"
-            href="https://www.instagram.com/busybeechildcare/" aria-label="instagram">
-                <i class="bi-instagram" style="font-size:36px; color:black;"></i>
-            </a>
-            <a class="btn bg-light" role="button"
-                href="https://www.youtube.com/channel/UCul5ckAkAQzN1M9RKyiaPMA" aria-label="youtube">
-                    <i class="bi-youtube" style="font-size:36px; color:black;"></i>
-            </a>
-            <a class="btn  bg-light" role="button"
-                href="https://twitter.com/busybee_pre" aria-label="twitter">
-                    <i class="bi-twitter" style="font-size:36px; color:black;"></i>
-            </a>
-            
-        </div>
+        <footer class="d-flex">
+            <div class="btn-group d-flex justify-content-center" role="group" aria-label="social media links">
+                <a class="btn" role="button"
+                href="https://www.facebook.com/BusyBeeChildCareMA" aria-label="facebook">
+                    <i class="bi-facebook" style="font-size:36px; color:black;"></i>
+                </a>
+                <a class="btn" role="button"
+                href="https://www.instagram.com/busybeechildcare/" aria-label="instagram">
+                    <i class="bi-instagram" style="font-size:36px; color:black;"></i>
+                </a>
+                <a class="btn" role="button"
+                    href="https://www.youtube.com/channel/UCul5ckAkAQzN1M9RKyiaPMA" aria-label="youtube">
+                        <i class="bi-youtube" style="font-size:36px; color:black;"></i>
+                </a>
+                <a class="btn" role="button"
+                    href="https://twitter.com/busybee_pre" aria-label="twitter">
+                        <i class="bi-twitter" style="font-size:36px; color:black;"></i>
+                </a>
+                
+            </div>
     </footer>
-    </div>
     </body>
 </html>
