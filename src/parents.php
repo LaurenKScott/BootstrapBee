9ce6c2aa39a6fc179f24e95cc9abf46e132a0af8
LaurenKScott <lkscottcom@gmail.com>
2022-03-10 15:35:57 -0500

    update pdf

--- src/parents.php
+++ src/parents.php
@@ -106,7 +106,7 @@ if (isset($_POST['password'])) {
     </header>
     <main class="container-fluid">
         <div class="mx-auto mt-3 d-flex justify-content-center">
-        <iframe src="/assets/Busy_Bee_Resources.pdf#view=Fit" width="90%" height="600px" frameborder="0"></iframe>
+        <iframe src="/assets/Busy_Bee_Resources.pdf#view=FitH" width="70%" height="600px" frameborder="0"></iframe>
         </div>
     <form title='login' method='POST' action='' autocomplete='off'>
         <div style='display:inline-block;'>
