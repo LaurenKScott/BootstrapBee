851780927b115947ea41c9f9fcf87a4aaeea00bc
LaurenKScott <lkscottcom@gmail.com>
2022-05-05 11:10:11 -0400

    background style

--- src/upload.php
+++ src/upload.php
@@ -1,14 +1,6 @@
 <?php 
 session_start();
-if (isset($_POST["submit"])){
-    $target_path = "/Users/lauren/Documents/PROJECTS/BootstrapBee/assets/uploads/";
-    $target_file = $target_path.basename( $_FILES['try_file']['name']);
-    if(move_uploaded_file($_FILES['try_file']['tmp_name'], $target_file)) {
-        echo "<h3 style='color:green;'>Upload successful</h3>";
-    }else {
-        echo "<h3>Upload failed</h3>";
-    }
-}
+
 ?>
 
 <!DOCTYPE html>
@@ -31,21 +23,115 @@ if (isset($_POST["submit"])){
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 </head>
 <body>
-    <div class="row text-center">
-        <h1 class="display-2">Busy Bee File Uploader</h1>
-    </div>
-    <div class="row">
-    <div class="text-center">
-        <form class="p-5 text-center" id="upload-form" 
-        action="<?php echo $_SERVER['PHP_SELF'];?>"  enctype="multipart/form-data" method="POST">
-            <div class="form-floating">
-                <input class="form-control" type="text" id="post-text" name="post-text" placeholder=""> 
-                <label for="post-text" class="form-label">Write something here...</label>
+    <header class="container-fluid">
+        <!-- NAVIGATION BAR WITH STICKY POSITION-->
+        <nav role="navigation" class="container-fluid navbar navbar-expand-sm navbar-light ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.html">
+                    <img src="/assets/busybeelogo.png" alt="Busy Bee Logo" 
+                    style="max-width:100%; height:auto;">
+                </a>
+                <button class="navbar-toggler mx-auto" type="button" data-bs-toggle="collapse" 
+                data-bs-target="#collapsible-navbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" 
+                id="collapsible-navbar">
+                    <ul class="navbar-nav" role="navigation">
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.html">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="programs.html">Programs</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="gallery.html">Gallery</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="parents.php">Parent Resources</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="faq.html">FAQ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact</a>
+                        </li>
+                    </ul>
             </div>
-            <input class="form-control" type="file" id="try_File" name="try_file">
-            <input type="submit" value="Submit" name="submit" id="upload-submit">
-        </form>
-    </div>
-    </div>
+            </div>
+        </nav>
+    <!-- END HEADER -->
+    </header>
+
+    <main>
+        <div class="row text-center">
+            <h1 class="display-2">Busy Bee File Uploader</h1>
+        </div>
+        <div class="row">
+            <p class="lead text-center"> Important: Be sure to review each file before posting
+            </p>
+        <div class="text-center">
+            <form class="p-5 text-center" id="upload-form" 
+            action="<?php echo $_SERVER['PHP_SELF'];?>"  enctype="multipart/form-data" method="POST">
+                <div class="form-floating">
+                    <input class="form-control" type="text" id="post-text" name="post-text" placeholder=""> 
+                    <label for="post-text" class="form-label">Write something here...</label>
+                </div>
+                <input class="form-control" type="file" id="try_File" name="try_file">
+                <input type="submit" value="Submit" name="submit" id="upload-submit">
+            </form>
+        <?php 
+        if (isset($_POST["submit"])){
+            $target_path = "/Users/lauren/Documents/PROJECTS/BootstrapBee/assets/uploads/";
+            $target_file = $target_path.basename( $_FILES['try_file']['name']);
+            if(move_uploaded_file($_FILES['try_file']['tmp_name'], $target_file)) {
+                echo "<h3>Upload successful</h3>";
+            }else {
+                echo "<h3>Upload failed</h3>";
+            }
+        }
+        ?>
+        </div>
+        </div>
+    </main>
+    <footer class="mt-5">
+        <!-- SOCIALS -->
+        <div class="row">
+            <div class="col-sm-3 text-center">
+                <h6>Content</h6>
+            </div>
+            <div class="col-sm-6 text-center">
+                <ul class="btn-group" id="socials" role="group" aria-label="social media links">
+                    <a class="btn" role="button" id="fb"
+                    href="https://www.facebook.com/BusyBeeChildCareMA" target="_blank" aria-label="facebook">
+                        <i id="fb" class="bi-facebook"></i>
+                    </a>
+                    <a class="btn" role="button"
+                    href="https://www.instagram.com/busybeechildcare/" target="_blank" aria-label="instagram">
+                        <i id ="ig" class="bi-instagram"></i>
+                    </a>
+                    <a class="btn" role="button"
+                        href="https://www.youtube.com/channel/UCul5ckAkAQzN1M9RKyiaPMA" target="_blank" aria-label="youtube">
+                            <i id="yt" class="bi-youtube"></i>
+                    </a>
+                    <a class="btn" role="button"
+                        href="https://twitter.com/busybee_pre" target="_blank" aria-label="twitter">
+                            <i id="tw" class="bi-twitter"></i>
+                    </a>   
+                </ul>
+            </div>
+            <div class="col-sm-3"></div>
+            <div class="row">
+                <div class="col-sm-3"></div>
+                <!-- COPYRIGHT INFO -->
+                <div class="col-sm-6 text-center">
+                    <p class="text-muted">Copyright Busy Bee Preschool 2022</p>
+                </div>
+                <div class="col-sm-3"></div>
+            </div>
+        </div>
+        <!-- END SOICALS --> 
+
+    </footer>
 </body>
 </html>
